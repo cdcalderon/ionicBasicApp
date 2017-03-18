7f4b343e5a73779242a06c0dddb165df7cae4fce
carloscalderon <cdcalderon@gmail.com>
2017-03-18 04:05:45 -0500

    Wire up toggle settings UI functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {QuotePage} from "../pages/quote/quote";
 import {SettingsPage} from "../pages/settings/settings";
 import {TabsPage} from "../pages/tabs/tabs";
 import {QuotesService} from "../services/quotes";
+import {SettingsService} from "../services/settings";
 
 @NgModule({
   declarations: [
@@ -39,6 +40,7 @@ import {QuotesService} from "../services/quotes";
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
-              QuotesService]
+              QuotesService,
+              SettingsService]
 })
 export class AppModule {}
