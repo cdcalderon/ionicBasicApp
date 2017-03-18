00bde0b787e1bf5d8a422e8a3b256753f944a1d4
carloscalderon <cdcalderon@gmail.com>
2017-03-18 04:21:38 -0500

    Add Change Color using configuration

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -3,6 +3,7 @@ import { Quote } from '../../data/quote.interface';
 import { QuotesService } from '../../services/quotes';
 import {ModalController} from "ionic-angular";
 import {QuotePage} from "../quote/quote";
+import {SettingsService} from "../../services/settings";
 
 @Component({
   selector: 'page-favorites',
@@ -12,7 +13,8 @@ export class FavoritesPage {
   quotes: Quote[];
 
   constructor (private quotesService: QuotesService,
-               private modalCtrl: ModalController) {}
+               private modalCtrl: ModalController,
+               private settingsService: SettingsService) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
@@ -37,6 +39,10 @@ export class FavoritesPage {
     });
 
     this.quotes.splice(position, 1);
+  }
+
+  getBackground() {
+    return this.settingsService.isAltBackground() ? 'altQuoteBackground' : 'quoteBackground';
   }
 }
 
